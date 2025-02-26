1cc37a5a31c262b33742f43d38bf3759ef245220
Juan Carlos Altamirano <juanca.altamirano@bayteq.com>
2025-02-26 10:06:21 -0500

    fix observe intersecting directive

--- projects/ui/src/lib/observe-intersecting.directive.ts
+++ projects/ui/src/lib/observe-intersecting.directive.ts
@@ -6,36 +6,45 @@ import {
   OnInit,
   PLATFORM_ID,
   inject,
-  output,
   OutputEmitterRef,
+  output,
+  OnDestroy
 } from '@angular/core';
 
 @Directive({
   selector: '[ftObserveIntersecting]',
-  standalone: true,
+  standalone: true
 })
-export class ObserveIntersectingDirective implements OnInit {
+export class ObserveIntersectingDirective implements OnInit, OnDestroy {
   private element = inject(ElementRef);
   private readonly platformId = inject(PLATFORM_ID);
-  public ftObserveIntersectingOptions = input<{
+  private elementObserver: IntersectionObserver | null = null;
+
+  ftObserveIntersectingOptions = input<{
     root?: HTMLElement;
     rootMargin?: string;
     threshold?: number | number[];
   }>();
   public readonly event: OutputEmitterRef<boolean> = output({ alias: 'ftObserveIntersecting' });
-
+  
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
       if ('IntersectionObserver' in window) {
-        const elementObserver = new IntersectionObserver((entries) => {
+        this.elementObserver = new IntersectionObserver((entries) => {
           entries.forEach((entry: IntersectionObserverEntry) => {
             this.event.emit(entry.isIntersecting);
           });
         }, this.ftObserveIntersectingOptions());
-        elementObserver.observe(this.element.nativeElement);
+        this.elementObserver.observe(this.element.nativeElement);
       } else {
         console.error('ftObserveIntersecting not available in this browser.');
       }
+    }
+  }
+  ngOnDestroy(): void {
+    if (this.elementObserver) {
+      this.elementObserver.disconnect();
+      this.elementObserver = null;
     }
   }
 }
